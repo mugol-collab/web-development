ffe02d7463454784cc7831d5eb5c74e1f6eabeb6
juan <juan@ifitness.com.br>
2021-01-08 11:22:03 -0300

    http module example

---
+++ NodeJs/Mosh/app08_http/app.js
@@ -0,0 +1,23 @@
+
+const http = require('http'); 
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+}); 
+
+// const server = http.createServer(); 
+// server.on('connection', (socket) => {
+//     console.log('New connection...');
+// });
+
+server.listen(3010);
+
+console.log('Listening on port 3010...');
