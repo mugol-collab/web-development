becb05429a3a7a30c6336f2487e27d7e9e0ca6db
juan <juan@ifitness.com.br>
2021-01-15 16:50:50 -0300

    OOP in Js - example 1 - Mosh tutorial

---
+++ Javascript/MoshJs/OOP/app_oop01/index.js
@@ -0,0 +1,18 @@
+// Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log("draw");
+    }
+  };
+}
+const circle = createCircle(1);
+
+// Constructor Function
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
