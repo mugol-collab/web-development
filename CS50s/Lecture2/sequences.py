a5523add440ac97e4c4b111dc916e2308fc522d3
juan <juan@ifitness.com.br>
2021-02-17 19:03:24 -0300

    python sequences

--- CS50s/Lecture2/sequences.py
+++ CS50s/Lecture2/sequences.py
@@ -1,2 +1,13 @@
-name = "Harry"
-print(name[1])
+### strings 
+# name = "Harry"
+# print(name[1])
+
+### lists
+# names = ["Harry", "Ron", "Hermione"]
+# print(names[0])
+
+### tuplas
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
