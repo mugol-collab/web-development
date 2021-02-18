e235ea00c59b92b4ce1d34ebe287ff2320a5e24c
juan <juan@ifitness.com.br>
2021-02-18 19:05:10 -0300

    sequences, data structures and functions examples

---
+++ CS50s/Lecture2/dictionaries.py
@@ -0,0 +1,8 @@
+houses = {"Harry": "Gryffindor", "Draco": "Slythering"}
+
+houses["Hermione"] = "Gryffindor"
+
+# print(houses["Harry"])
+# print(houses["Hermione"])
+# print(houses["Draco"])
+print(houses)
---
+++ CS50s/Lecture2/functions.py
@@ -0,0 +1,6 @@
+# from squares import square
+import squares 
+
+for i in range(10): 
+    # print(f"The square of {i} is {square(i)}")
+    print(f"The square of {i} is {squares.square(i)}")
