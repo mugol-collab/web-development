5979358324fde0a2cf25d80db9f6ce7fddbc1d62
juan <juan@ifitness.com.br>
2021-02-24 20:36:18 -0300

    NLW#4 - aula 02 - Banco de Dados

--- NodeJs/NLW4/aulas/api/src/server.ts
+++ NodeJs/NLW4/aulas/api/src/server.ts
@@ -1,29 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
+import "./database"
+import { router } from "./routes";
 
 const app = express();
 
-/**
- * GET => Busca
- * POST => Salvar 
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteracao especifica 
- */
-
- //http://localhost:3333/users
-
- app.get("/", (request, response) => {
-    // return response.send("Hello World - NLW04");
-    return response.json({message: "Hello World - NLW04"});
- });
-
-// 1 param => rota (Recurso API)
-// 2 param => request, response
-
- app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"});
- });
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
     console.log("Server is running!");
