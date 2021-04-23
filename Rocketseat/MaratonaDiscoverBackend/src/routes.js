c974acf92f6af137367383c30532d1eef05556f6
Juan <juan_jg@hotmail.com>
2021-04-22 23:52:32 -0300

    inicio aula 02 maratona Discover

--- Rocketseat/MaratonaDiscoverBackend/src/routes.js
+++ Rocketseat/MaratonaDiscoverBackend/src/routes.js
@@ -5,7 +5,8 @@ const views = __dirname + "/views/";
 
 const profile = {
     name: "Juan",
-    avatar: "https://avatars.githubusercontent.com/u/57006673?v=4",
+    // avatar: "https://avatars.githubusercontent.com/u/57006673?v=4",
+    avatar: "https://github.com/mugol-collab.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
@@ -15,6 +16,9 @@ const profile = {
 // req, res
 routes.get('/', (req, res) => res.render(views + "index"));
 routes.get('/job', (req, res) => res.render(views + "job"));
+routes.post('/job', (req, res) => {
+    console.log(req.body);
+});
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"));
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }));
 
